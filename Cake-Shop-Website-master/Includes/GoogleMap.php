d059fb78892ea4f65ed5adb723324e95999a0ffd
utka12vjko <104752045+utka12vjko@users.noreply.github.com>
2022-05-02 12:02:01 +0530

    myangel bakery shop webiste

---
+++ Cake-Shop-Website-master/Includes/GoogleMap.php
@@ -0,0 +1,12 @@
+<?php include "./AdditionalPHP/checkAccess.php"; ?>
+
+<div class="map">
+    <div class="subtitle">
+        <h2>SHOP LOCATE US</h2>
+    </div>
+
+    <div class="google-map">
+        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7483.163979214587!2d57.5197261228079!3d-20.31756231290802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x217c5c4272c436d1%3A0x14c5afb30243ca4a!2sPatisserie%20Swiss%20At%20Jerningham%20and%20Royal%20Street!5e0!3m2!1sen!2smu!4v1606498197938!5m2!1sen!2smu" width="100%"" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.9470904661675!2d72.85447031443786!3d19.022052987118844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cf269094f8e7%3A0x9c6b0aed51adda90!2sV.J.T.I.!5e0!3m2!1sen!2sin!4v1638253761966!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+</div>
